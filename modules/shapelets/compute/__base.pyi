caf4743ae33c3f792e58551204f710209175de95
Justo Ruiz Ferrer <justo.ruiz@shapelets.io>
2021-03-08 00:28:35 +0100

    Remove comments and they don't seem to affect anything

--- modules/shapelets/compute/__base.pyi
+++ modules/shapelets/compute/__base.pyi
@@ -2,19 +2,6 @@ from typing import Optional, Tuple, Union, overload
 from .__base import (Number, Shape, DataType, ArrayLike)
 
 class Backend:
-    """
-    Defines the different computational backends where computations are executed
-
-    Members:
-
-      Default : It would resolve to the first available backend out of CUDA, CPU and OpenCL.
-
-      CPU : Uses CPU multicore capabilities.
-
-      CUDA : Executes algorithms in CUDA devices.
-
-      OpenCL : Executes algorithms in OpenCL devices.
-    """
     def __eq__(self, arg0: object) -> bool: ...
     def __getstate__(self) -> int: ...
     def __hash__(self) -> int: ...
@@ -24,10 +11,7 @@ class Backend:
     def __repr__(self) -> str: ...
     def __setstate__(self, arg0: int) -> None: ...
     @property
-    def name(self) -> str:
-        """
-        :type: str
-        """
+    def name(self) -> str:...
     CPU: shapelets.compute._pygauss.Backend # value = <Backend.CPU: 1>
     CUDA: shapelets.compute._pygauss.Backend # value = <Backend.CUDA: 2>
     Default: shapelets.compute._pygauss.Backend # value = <Backend.Default: 0>
@@ -35,19 +19,6 @@ class Backend:
     __members__: dict # value = {'Default': <Backend.Default: 0>, 'CPU': <Backend.CPU: 1>, 'CUDA': <Backend.CUDA: 2>, 'OpenCL': <Backend.OpenCL: 4>}
 
 class BorderType:
-    """
-    Border type
-
-    Members:
-
-      Zero : Values are 0
-
-      Symmetric : Values are symmetric over the edge.
-
-      ClampEdge : Values are clamped to the edge.
-
-      Periodic : Values are mapped to range of the dimension in cyclic fashion.
-    """
     def __eq__(self, arg0: object) -> bool: ...
     def __getstate__(self) -> int: ...
     def __hash__(self) -> int: ...
@@ -57,10 +28,7 @@ class BorderType:
     def __repr__(self) -> str: ...
     def __setstate__(self, arg0: int) -> None: ...
     @property
-    def name(self) -> str:
-        """
-        :type: str
-        """
+    def name(self) -> str:...
     ClampEdge: shapelets.compute._pygauss.BorderType # value = <BorderType.ClampEdge: 2>
     Periodic: shapelets.compute._pygauss.BorderType # value = <BorderType.Periodic: 3>
     Symmetric: shapelets.compute._pygauss.BorderType # value = <BorderType.Symmetric: 1>
@@ -68,17 +36,6 @@ class BorderType:
     __members__: dict # value = {'Zero': <BorderType.Zero: 0>, 'Symmetric': <BorderType.Symmetric: 1>, 'ClampEdge': <BorderType.ClampEdge: 2>, 'Periodic': <BorderType.Periodic: 3>}
 
 class ConvDomain:
-    """
-    Convolution Domain
-
-    Members:
-
-      Auto : Automatically picks the right convolution algorithm
-
-      Frequency : Perform convolution in frequency domain
-
-      Spatial : Perform convolution in spatial domain
-    """
     def __eq__(self, arg0: object) -> bool: ...
     def __getstate__(self) -> int: ...
     def __hash__(self) -> int: ...
@@ -88,25 +45,13 @@ class ConvDomain:
     def __repr__(self) -> str: ...
     def __setstate__(self, arg0: int) -> None: ...
     @property
-    def name(self) -> str:
-        """
-        :type: str
-        """
+    def name(self) -> str:...
     Auto: shapelets.compute._pygauss.ConvDomain # value = <ConvDomain.Auto: 0>
     Frequency: shapelets.compute._pygauss.ConvDomain # value = <ConvDomain.Frequency: 2>
     Spatial: shapelets.compute._pygauss.ConvDomain # value = <ConvDomain.Spatial: 1>
     __members__: dict # value = {'Auto': <ConvDomain.Auto: 0>, 'Frequency': <ConvDomain.Frequency: 2>, 'Spatial': <ConvDomain.Spatial: 1>}
 
 class ConvMode:
-    """
-    Convolution Mode
-
-    Members:
-
-      Default : Output of the convolution is the same size as input
-
-      Expand : Output of the convolution is signal_len + filter_len - 1
-    """
     def __eq__(self, arg0: object) -> bool: ...
     def __getstate__(self) -> int: ...
     def __hash__(self) -> int: ...
@@ -116,157 +61,49 @@ class ConvMode:
     def __repr__(self) -> str: ...
     def __setstate__(self, arg0: int) -> None: ...
     @property
-    def name(self) -> str:
-        """
-        :type: str
-        """
+    def name(self) -> str:...
     Default: shapelets.compute._pygauss.ConvMode # value = <ConvMode.Default: 0>
     Expand: shapelets.compute._pygauss.ConvMode # value = <ConvMode.Expand: 1>
     __members__: dict # value = {'Default': <ConvMode.Default: 0>, 'Expand': <ConvMode.Expand: 1>}
 
 class DeviceInfo:
-    """
-            Data class describing a device where computations are run within the active backend.
-
-            A computational backend may expose more than one device.  Use ::func::`~shapelets.get_devices` to
-            get a complete list of devices found on the active backend.
-            
-    """
     def __repr__(self) -> str: ...
     @property
-    def compute(self) -> str:
-        """
-        Compute capabilities of the device within the platform
-
-        :type: str
-        """
+    def compute(self) -> str: ...
     @property
-    def id(self) -> int:
-        """
-        Id for this device, which is unique within the backend.
-
-        :type: int
-        """
+    def id(self) -> int: ...
     @property
-    def isDoubleAvailable(self) -> bool:
-        """
-        Returns true if Float64 is supported.
-
-        :type: bool
-        """
+    def isDoubleAvailable(self) -> bool: ...
     @property
-    def isHalfAvailable(self) -> bool:
-        """
-        Returns true if Float16 is supported.
-
-        :type: bool
-        """
+    def isHalfAvailable(self) -> bool: ...
     @property
-    def name(self) -> str:
-        """
-        Descriptive device name as provided by the drivers in this system.
-
-        :type: str
-        """
+    def name(self) -> str: ...
     @property
-    def platform(self) -> str:
-        """
-        Platform information associated to the backend and device.
-
-        :type: str
-        """
+    def platform(self) -> str: ...
 
 class DeviceMemory:
-    """
-            Describes how much memory is currently in used on a particular device.
-
-            Use ::func::`~shapelets.get_device_memory` to report and populate memory usage. The function
-            ::func::`~shapelets.device_gc` will force a synchronization and removal of temporal arrays.
-            
-    """
     def __repr__(self) -> str: ...
     @property
-    def buffers(self) -> int:
-        """
-        Number of distinct buffers in use.
-
-        :type: int
-        """
+    def buffers(self) -> int: ...
     @property
-    def bytes(self) -> int:
-        """
-        Number of bytes used.
-
-        :type: int
-        """
+    def bytes(self) -> int: ...
     @property
-    def locked_buffers(self) -> int:
-        """
-        Number of distinct buffers currently locked.
-
-        :type: int
-        """
+    def locked_buffers(self) -> int: ...
     @property
-    def locked_bytes(self) -> int:
-        """
-        Number of bytes currently locked.
-
-        :type: int
-        """
+    def locked_bytes(self) -> int: ...
 
 class MatrixProfile:
     def __init__(self, arg0: ShapeletsArray, arg1: ShapeletsArray) -> None: ...
     @property
-    def index(self) -> ShapeletsArray:
-        """
-        :type: ShapeletsArray
-        """
+    def index(self) -> ShapeletsArray: ...
     @index.setter
-    def index(self, arg0: ShapeletsArray) -> None:
-        """
-        TODO
-        """
+    def index(self, arg0: ShapeletsArray) -> None: ...
     @property
-    def profile(self) -> ShapeletsArray:
-        """
-        :type: ShapeletsArray
-        """
+    def profile(self) -> ShapeletsArray: ...
     @profile.setter
-    def profile(self, arg0: ShapeletsArray) -> None:
-        """
-        TODO
-        """
+    def profile(self, arg0: ShapeletsArray) -> None: ...
 
 class MatrixProperties:
-    """
-    Matrix Properties
-
-    Members:
-
-      Default : Default
-
-      Transposed : Data needs to be transposed
-
-      ConjugatedTransposed : Data needs to be conjugated transposed
-
-      Conjugated : Data needs to be conjugate
-
-      Upper : Matrix is upper triangular
-
-      Lower : Matrix is lower triangular
-
-      UnitDiagonal : Matrix diagonal contains unitary values
-
-      Symmetric : Matrix is symmetric
-
-      PositiveDefinite : Matrix is positive definite
-
-      Orthogonal : Matrix is orthogonal
-
-      TriDiagonal : Matrix is tri-diagonal
-
-      BlockDiagonal : Matrix is block diagonal
-    """
     def __eq__(self, arg0: object) -> bool: ...
     def __getstate__(self) -> int: ...
     def __hash__(self) -> int: ...
@@ -276,11 +113,7 @@ class MatrixProperties:
     def __repr__(self) -> str: ...
     def __setstate__(self, arg0: int) -> None: ...
     @property
-    def name(self) -> str:
-        """
-        :type: str
-        """
-
+    def name(self) -> str:...
     BlockDiagonal: shapelets.compute._pygauss.MatrixProperties # value = <MatrixProperties.Symmetric: 512>
     Conjugated: shapelets.compute._pygauss.MatrixProperties # value = <MatrixProperties.Conjugated: 4>
     ConjugatedTransposed: shapelets.compute._pygauss.MatrixProperties # value = <MatrixProperties.ConjugatedTransposed: 2>
@@ -296,29 +129,6 @@ class MatrixProperties:
     __members__: dict # value = {'Default': <MatrixProperties.Default: 0>, 'Transposed': <MatrixProperties.Transposed: 1>, 'ConjugatedTransposed': <MatrixProperties.ConjugatedTransposed: 2>, 'Conjugated': <MatrixProperties.Conjugated: 4>, 'Upper': <MatrixProperties.Upper: 32>, 'Lower': <MatrixProperties.Lower: 64>, 'UnitDiagonal': <MatrixProperties.UnitDiagonal: 128>, 'Symmetric': <MatrixProperties.Symmetric: 512>, 'PositiveDefinite': <MatrixProperties.PositiveDefinite: 1024>, 'Orthogonal': <MatrixProperties.Orthogonal: 2048>, 'TriDiagonal': <MatrixProperties.TriDiagonal: 4096>, 'BlockDiagonal': <MatrixProperties.Symmetric: 512>}
 
 class NormType:
-    """
-    Norm Type
-
-    Members:
-
-      Vector1 : Treats the input as a vector and returns the sum of absolute values
-
-      VectorInf : Treats the input as a vector and returns the max of absolute values
-
-      Vector2 : Treats the input as a vector and returns euclidean norm
-
-      VectorP : Treats the input as a vector and returns the p-norm
-
-      Matrix : Return the max of column sums
-
-      MatrixInf : Return the max of row sums
-
-      Singular : Returns the max singular value). Currently NOT SUPPORTED
-
-      LPQ : Returns Lpq-norm
-
-      Euclid : Same as Vector2
-    """
     def __eq__(self, arg0: object) -> bool: ...
     def __getstate__(self) -> int: ...
     def __hash__(self) -> int: ...
@@ -328,10 +138,7 @@ class NormType:
     def __repr__(self) -> str: ...
     def __setstate__(self, arg0: int) -> None: ...
     @property
-    def name(self) -> str:
-        """
-        :type: str
-        """
+    def name(self) -> str: ...
     Euclid: shapelets.compute._pygauss.NormType # value = <NormType.Vector2: 2>
     LPQ: shapelets.compute._pygauss.NormType # value = <NormType.LPQ: 7>
     Matrix: shapelets.compute._pygauss.NormType # value = <NormType.Matrix: 4>
@@ -348,19 +155,6 @@ class ParallelFor:
     def __next__(self) -> ParallelFor: ...
 
 class ScanOp:
-    """
-    Scan binary operations
-
-    Members:
-
-      Add : 
-
-      Mul : 
-
-      Min : 
-
-      Max : 
-    """
     def __eq__(self, arg0: object) -> bool: ...
     def __getstate__(self) -> int: ...
     def __hash__(self) -> int: ...
@@ -370,10 +164,7 @@ class ScanOp:
     def __repr__(self) -> str: ...
     def __setstate__(self, arg0: int) -> None: ...
     @property
-    def name(self) -> str:
-        """
-        :type: str
-        """
+    def name(self) -> str: ...
     Add: shapelets.compute._pygauss.ScanOp # value = <ScanOp.Add: 0>
     Max: shapelets.compute._pygauss.ScanOp # value = <ScanOp.Max: 3>
     Min: shapelets.compute._pygauss.ScanOp # value = <ScanOp.Min: 2>
@@ -387,14 +178,8 @@ class ScopedBatch:
 class ShapeletsArray:
     def __add__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __and__(self, other: ArrayLike) -> ShapeletsArray: ...
-    def __copy__(self) -> ShapeletsArray: 
-        """
-        Shallow copy
-        """
-    def __deepcopy__(self, memo: object) -> ShapeletsArray: 
-        """
-        Deep copy
-        """
+    def __copy__(self) -> ShapeletsArray: ...
+    def __deepcopy__(self, memo: object) -> ShapeletsArray: ...
     def __eq__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __ge__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __getitem__(self, selector: object) -> ShapeletsArray: ...
@@ -413,10 +198,7 @@ class ShapeletsArray:
     def __le__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __lshift__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __lt__(self, other: ArrayLike) -> ShapeletsArray: ...
-    def __matmul__(self, other: ArrayLike) -> ShapeletsArray: 
-        """
-        Matrix multiplication
-        """
+    def __matmul__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __mod__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __mul__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __ne__(self, other: ArrayLike) -> ShapeletsArray: ...
@@ -427,10 +209,7 @@ class ShapeletsArray:
     def __rand__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __repr__(self) -> str: ...
     def __rlshift__(self, other: ArrayLike) -> ShapeletsArray: ...
-    def __rmatmul__(self, other: ArrayLike) -> ShapeletsArray: 
-        """
-        Matrix multiplication
-        """
+    def __rmatmul__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __rmod__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __rmul__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __ror__(self, other: ArrayLike) -> ShapeletsArray: ...
@@ -444,87 +223,33 @@ class ShapeletsArray:
     def __sub__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __truediv__(self, other: ArrayLike) -> ShapeletsArray: ...
     def __xor__(self, other: ArrayLike) -> ShapeletsArray: ...
-    def astype(self, type: DataType) -> ShapeletsArray: 
-        """
-        converts the array into a new array with the specified type
-        """
+    def astype(self, type: DataType) -> ShapeletsArray: ...
     def display(self, precision: int = 4, transpose: bool = True) -> None: ...
-    def same_as(self, arr_like: ArrayLike, eps: float_ = 0.0001) -> bool: 
-        """
-        Performs a element wise comparison between the arrays and returns True if the two arrays are the same (same dimensions, same values).
-        """
+    def same_as(self, arr_like: ArrayLike, eps: float_ = 0.0001) -> bool: ...
     @property
-    def H(self) -> ShapeletsArray:
-        """
-        Get the conjugate-transpose of the current array
-
-        :type: ShapeletsArray
-        """
+    def H(self) -> ShapeletsArray:...
     @property
-    def T(self) -> ShapeletsArray:
-        """
-        Get the transposed the array
-
-        :type: ShapeletsArray
-        """
+    def T(self) -> ShapeletsArray:...
     @property
-    def backend(self) -> Backend:
-        """
-        :type: Backend
-        """
+    def backend(self) -> Backend: ...
     @property
-    def dtype(self) -> DataType:
-        """
-        Returns the numpy dtype describing the type of elements held by this array
-
-        :type: DataType
-        """
+    def dtype(self) -> DataType: ...
     @property
-    def is_column(self) -> bool:
-        """
-        :type: bool
-        """
+    def is_column(self) -> bool: ...
     @property
-    def is_row(self) -> bool:
-        """
-        :type: bool
-        """
+    def is_row(self) -> bool: ...
     @property
-    def is_single(self) -> bool:
-        """
-        :type: bool
-        """
+    def is_single(self) -> bool: ...
     @property
-    def is_vector(self) -> bool:
-        """
-        :type: bool
-        """
+    def is_vector(self) -> bool: ...
     @property
-    def itemsize(self) -> int:
-        """
-        Returns the size in bytes of each individual item held by this array
-
-        :type: int
-        """
+    def itemsize(self) -> int: ...
     @property
-    def ndim(self) -> int:
-        """
-        Returns the number of dimensions.
-
-        :type: int
-        """
+    def ndim(self) -> int: ...
     @property
-    def shape(self) -> Shape:
-        """
-        :type: Shape
-        """
+    def shape(self) -> Shape: ...
     @property
-    def size(self) -> int:
-        """
-        Returns the total number of elements of this array.
-
-        :type: int
-        """
+    def size(self) -> int: ...
     __array_priority__ = 30
     __hash__ = None
 
--- modules/shapelets/compute/random.pyi
+++ modules/shapelets/compute/random.pyi
@@ -2,19 +2,6 @@ from typing import Optional, Tuple, Union, overload
 from .__base import ShapeletsArray, DataType, Shape
 
 class RandomEngineType():
-    """
-    Built-in engines for random number generation
-
-    Members:
-
-      Default : Defaults to Philox
-
-      Mersenne : Mersenne GP 11213.
-
-      Threefry : Threefry 2X32_16.
-
-      Philox : Philox 4x32_10.
-    """
     def __eq__(self, arg0: object) -> bool: ...
     def __getstate__(self) -> int: ...
     def __hash__(self) -> int: ...
@@ -24,10 +11,7 @@ class RandomEngineType():
     def __repr__(self) -> str: ...
     def __setstate__(self, arg0: int) -> None: ...
     @property
-    def name(self) -> str:
-        """
-        :type: str
-        """
+    def name(self) -> str:...
     Default: shapelets.compute._pygauss.RandomEngineType # value = <RandomEngineType.Default: 100>
     Mersenne: shapelets.compute._pygauss.RandomEngineType # value = <RandomEngineType.Mersenne: 300>
     Philox: shapelets.compute._pygauss.RandomEngineType # value = <RandomEngineType.Default: 100>
@@ -35,70 +19,19 @@ class RandomEngineType():
     __members__: dict # value = {'Default': <RandomEngineType.Default: 100>, 'Mersenne': <RandomEngineType.Mersenne: 300>, 'Threefry': <RandomEngineType.Threefry: 200>, 'Philox': <RandomEngineType.Default: 100>}
 
 class ShapeletsRandomEngine():
-    def beta(self, a: float, b: float, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: 
-        """
-        Draw samples from a Beta distribution.  Alpha is what is called shape or K parameter of the Gamma distribution
-        """
-    def chisquare(self, df: float, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: 
-        """
-        Draw samples from a chi-square distribution.  df is the degree of freedom
-        """
-    def exponential(self, scale: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: 
-        """
-        Draw samples from an exponential distribution.  Scale is the inverse of lambda.
-        """
-    def gamma(self, alpha: float, scale: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: 
-        """
-        Draw samples from a Gamma distribution.  Alpha is what is called shape or K parameter of the Gamma distribution
-        """
-    def logistic(self, loc: float = 0.0, scale: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: 
-        """
-        Draw samples from a logistic distribution.
-        """
-    def lognormal(self, mean: float = 0.0, sigma: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: 
-        """
-        Draw samples from a log-normal distribution.
-        """
-    def multivariate_normal(self, mean: object, cov: object, samples: int, dtype: DataType = 'float32') -> ShapeletsArray: 
-        """
-        Draw samples from a logistic distribution.
-        """
-    def normal(self, loc: float = 0.0, scale: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: 
-        """
-        Draw random samples from a normal (Gaussian) distribution.
-        """
-    def standard_normal(self, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: 
-        """
-        Draw random samples from a normal (Gaussian) distribution.
-        """
-    def uniform(self, low: float = 0.0, high: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: 
-        """
-        Samples are uniformly distributed over the half-open interval [low, high)
-        """
-    def wald(self, mean: float, scale: float, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: 
-        """
-        Draw samples from a Wald, or inverse Gaussian, distribution.
-        """
+    def beta(self, a: float, b: float, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: ...
+    def chisquare(self, df: float, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: ...
+    def exponential(self, scale: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: ...
+    def gamma(self, alpha: float, scale: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: ...
+    def logistic(self, loc: float = 0.0, scale: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: ...
+    def lognormal(self, mean: float = 0.0, sigma: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: ...
+    def multivariate_normal(self, mean: object, cov: object, samples: int, dtype: DataType = 'float32') -> ShapeletsArray: ...
+    def normal(self, loc: float = 0.0, scale: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: ...
+    def standard_normal(self, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: ...
+    def uniform(self, low: float = 0.0, high: float = 1.0, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: ...
+    def wald(self, mean: float, scale: float, shape: Shape = (1, 1), dtype: DataType = 'float32') -> ShapeletsArray: ...
 
-def default_rng(type: RandomEngineType = RandomEngineType.Default, seed: int = 0) -> ShapeletsRandomEngine:
-    """
-    Creates a new random engine
-    """
-
-def randn(shape: Shape, dtype: DataType = 'float32', engine: typing.Optional[ShapeletsRandomEngine] = None) -> ShapeletsArray:
-    """
-    Creates a new array using random numbers drawn from a normal distribution
-    """
-
-def random(shape: Shape, dtype: DataType = 'float32', engine: Optional[ShapeletsRandomEngine] = None) -> ShapeletsArray:
-    """
-    Creates a new array using random values drawn from a uniform distribution
-    """    
-
-def randint(low: int, high: Optional[int] = None, shape: Shape = (1,1), dtype: DataType = 'float32', engine: Optional[ShapeletsRandomEngine] = None) -> ShapeletsArray:
-    """
-    Return random integers from low (inclusive) to high (exclusive)
-
-    Returns random integers from the discrete uniform distribution of the specified dtype in the half-open
-    interval [low, high). If high is None (the default), then results are from [0, low).
-    """
+def default_rng(type: RandomEngineType = RandomEngineType.Default, seed: int = 0) -> ShapeletsRandomEngine:...
+def randn(shape: Shape, dtype: DataType = 'float32', engine: typing.Optional[ShapeletsRandomEngine] = None) -> ShapeletsArray:...
+def random(shape: Shape, dtype: DataType = 'float32', engine: Optional[ShapeletsRandomEngine] = None) -> ShapeletsArray:...
+def randint(low: int, high: Optional[int] = None, shape: Shape = (1,1), dtype: DataType = 'float32', engine: Optional[ShapeletsRandomEngine] = None) -> ShapeletsArray:...
