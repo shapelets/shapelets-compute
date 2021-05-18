d479837db21c633af349e0697058b834569fba09
Justo Ruiz Ferrer <justo.ruiz@shapelets.io>
2021-05-18 15:21:20 +0200

    make more resilient type definitions in 3.7

--- modules/shapelets/compute/__basic_typing.py
+++ modules/shapelets/compute/__basic_typing.py
@@ -1,15 +1,37 @@
 from __future__ import annotations
-from typing import Sequence, Union, Tuple
-import numpy.typing as npt
+from typing import Sequence, Union, Tuple, Any, overload
+import sys
+import numpy as np
 
-_BoolLike = npt._BoolLike
-_IntLike = npt._IntLike
-_FloatLike = npt._FloatLike
-_ComplexLike = npt._ComplexLike
-_NumberLike = npt._NumberLike
-_ScalarLike = npt._ScalarLike
+if sys.version_info >= (3, 8):
+    from typing import Protocol
+    HAVE_PROTOCOL = True
+else:
+    try:
+        from typing_extensions import Protocol
+    except ImportError:
+        HAVE_PROTOCOL = False
+    else:
+        HAVE_PROTOCOL = True
 
-ArrayLike = npt.ArrayLike
-DataTypeLike = npt.DTypeLike
+if HAVE_PROTOCOL:
+    class _SupportsArray(Protocol):
+        @overload
+        def __array__(self, __dtype: DTypeLike = ...) -> np.ndarray: ...
+        @overload
+        def __array__(self, dtype: DTypeLike = ...) -> np.ndarray: ...
+else:
+    _SupportsArray = Any
+
+_BoolLike = Union[bool, np.bool_]
+_IntLike = Union[int, np.integer]
+_FloatLike = Union[_IntLike, float, np.floating]
+_ComplexLike = Union[_FloatLike, complex, np.complexfloating]
+_NumberLike = Union[int, float, complex, np.number, np.bool_]
+_ScalarLike = _ScalarLike = Union[int, float, complex, bytes, np.generic]
+
+ArrayLike = Union[_ScalarLike, Sequence[_ScalarLike], Sequence[Sequence[Any]], _SupportsArray]
+DTypeLike = Union[np.dtype, None, str]
 Shape = Tuple[int, ...]
 ShapeLike = Union[int, Sequence[int]]
+
