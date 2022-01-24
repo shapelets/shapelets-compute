8f53d98692dad31ed4a2d94846318c329f3ed4fd
Miquel Collado <miquel.collado@shapelets.io>
2022-01-24 10:36:01 +0100

    move LDFLAGS to the entrypoint

--- entrypoint-build-wheels.sh
+++ entrypoint-build-wheels.sh
@@ -3,6 +3,7 @@
 yum -y install zip
 pipx install tox 
 cd io
+export LDFLAGS=-L/usr/local/cuda/lib64
 tox
 for whl in `ls dist/*.whl | grep -v manylinux`; do auditwheel repair $whl -w dist/; rm $whl; done
 
