eb5312608141c5bb272e4fb6b3b232aa33fc3481
Miquel Collado <miquel.collado@shapelets.io>
2022-02-10 14:08:21 +0100

    auditwheel patch

--- auditwheel_patch.py
+++ auditwheel_patch.py
@@ -6,7 +6,7 @@ from auditwheel.policy import _POLICIES as POLICIES
 
 # libs loaded dynamically; do not include it
 for p in POLICIES:
-    p['lib_whitelist'].append('libcufft.so')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
+    #p['lib_whitelist'].append('libcufft.so')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
     # p['lib_whitelist'].append('libcufft-da2333af.so.10.5.0.43')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
     # p['lib_whitelist'].append('libcufft-da2333af.so')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
 
@@ -17,7 +17,7 @@ for p in POLICIES:
     # p['lib_whitelist'].append('libcufft.so.10.5.0.43')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
     # p['lib_whitelist'].append('libcufft.so.10.5.0')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
     # p['lib_whitelist'].append('libcufft.so.10.5')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
-    # p['lib_whitelist'].append('libcufft.so.10')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
+    p['lib_whitelist'].append('libcufft.so.10')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
 
 if __name__ == "__main__":
     sys.exit(main())
