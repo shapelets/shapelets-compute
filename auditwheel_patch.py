a18c00c2ae4756ee1b990199bb4c46761064a7de
Miquel Collado <miquel.collado@shapelets.io>
2022-02-10 10:41:27 +0100

    auditwheel patch

---
+++ auditwheel_patch.py
@@ -0,0 +1,12 @@
+# Monkey patch to not ship libjvm.so in pypi wheels
+import sys
+
+from auditwheel.main import main
+from auditwheel.policy import _POLICIES as POLICIES
+
+# libjvm is loaded dynamically; do not include it
+for p in POLICIES:
+    p['lib_whitelist'].append('libcufft.so')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
+
+if __name__ == "__main__":
+    sys.exit(main())
