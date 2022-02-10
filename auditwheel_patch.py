6bafcd216570f51f5604d2811da5fe8baf781dae
Miquel Collado <miquel.collado@shapelets.io>
2022-02-10 13:08:12 +0100

    auditwheel patch

--- auditwheel_patch.py
+++ auditwheel_patch.py
@@ -7,6 +7,7 @@ from auditwheel.policy import _POLICIES as POLICIES
 # libs loaded dynamically; do not include it
 for p in POLICIES:
     p['lib_whitelist'].append('libcufft.so')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
+    p['lib_whitelist'].append('libcufft-da2333af.so.10.5.0.43')  # shapelets_compute.libs/libcufft-da2333af.so.10.5.0.43
 
 if __name__ == "__main__":
     sys.exit(main())
