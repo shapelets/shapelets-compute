84be9ccb4a873b6720000a2e442d1c151fd53e72
Miquel Collado <miquel.collado@shapelets.io>
2022-02-10 11:12:12 +0100

    auditwheel patch

--- entrypoint-build-wheels.sh
+++ entrypoint-build-wheels.sh
@@ -11,5 +11,5 @@ tox
 export AUDITWHEEL=`pwd`/auditwheel_patch.py  # the monkey patch script
 
 # for whl in `ls dist/*.whl | grep -v manylinux`; do auditwheel repair $whl -w dist/; rm $whl; done
-for whl in `ls dist/*.whl | grep -v manylinux`; do python $AUDITWHEEL repair $whl -w dist/; done
+for whl in `ls dist/*.whl | grep -v manylinux`; do python3 $AUDITWHEEL repair $whl -w dist/; done
 
