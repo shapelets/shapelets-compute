ae37d94f70a9823d4c8871ad38297565ad9eb440
Miquel Collado <miquel.collado@shapelets.io>
2022-02-10 11:14:30 +0100

    auditwheel patch

--- entrypoint-build-wheels.sh
+++ entrypoint-build-wheels.sh
@@ -3,11 +3,12 @@
 yum -y install zip
 pipx install tox
 pipx install twine
-pip3 install auditwheel
+
 cd io
 export LDFLAGS=-L/usr/local/cuda/lib64
 tox
 
+pip3 install auditwheel
 export AUDITWHEEL=`pwd`/auditwheel_patch.py  # the monkey patch script
 
 # for whl in `ls dist/*.whl | grep -v manylinux`; do auditwheel repair $whl -w dist/; rm $whl; done
